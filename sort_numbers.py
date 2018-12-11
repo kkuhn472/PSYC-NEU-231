2f16af73ecc82230bc7a28fc302f5a6a8ab4a145
kkuhn472 <43707680+kkuhn472@users.noreply.github.com>
2018-12-11 14:46:43 -0800

    Finished all the exercises (hopefully)

---
+++ sort_numbers.py
@@ -0,0 +1,21 @@
+
+def sort_numbers_low_high (list):
+    x = list[0]
+    y = list[1]
+    new_list = []
+    
+    if type(x) is int and if type(y) is int:
+        if x > y:
+            return [x,y]
+        else
+            return [y,x]
+    else
+        return 'Please enter an integer'
+    
+
+def multiply_array (list):
+    out = 1
+    for i in range(0,len(list)):
+        out = out * list[i]
+    return result
+    print(result)
